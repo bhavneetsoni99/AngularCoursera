437f7c4952de6b57aba6e6b26e0fa89ad32b0033
Bhavneet Soni <bhavneeetsoni@gmail.com>
2020-04-11 16:01:06 -0500

    data binding

---
+++ conFusion/src/app/dish-detail/dish-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from "@angular/core";
+import { Dish } from "./../shared/dish";
+@Component({
+  selector: "app-dish-detail",
+  templateUrl: "./dish-detail.component.html",
+  styleUrls: ["./dish-detail.component.scss"],
+})
+export class DishDetailComponent implements OnInit {
+  constructor() {}
+
+  @Input()
+  dish: Dish;
+
+  ngOnInit(): void {}
+}
