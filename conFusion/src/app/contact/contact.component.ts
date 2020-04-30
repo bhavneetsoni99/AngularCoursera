c6fbbff49261c422dc8c5d3f7dcf3c03a227aedb
Bhavneet Soni <bhavneeetsoni@gmail.com>
2020-04-30 13:36:59 -0500

    added reactive form input for contact us

--- conFusion/src/app/contact/contact.component.ts
+++ conFusion/src/app/contact/contact.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Feedback, ContactType } from "./../shared/feedback";
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  selector: "app-contact",
+  templateUrl: "./contact.component.html",
+  styleUrls: ["./contact.component.scss"],
 })
 export class ContactComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  feedbackForm: FormGroup;
+  feedback: Feedback;
+  contactType = ContactType;
+  constructor(private fb: FormBuilder) {
+    this.createForm();
   }
 
+  ngOnInit(): void {}
+  createForm() {
+    this.feedbackForm = this.fb.group({
+      firstName: "",
+      lastname: "",
+      telnum: 0,
+      email: "",
+      agree: false,
+      contacttype: "None",
+      message: "",
+    });
+  }
+  onSubmit() {
+    this.feedback = this.feedbackForm.value;
+    console.log(this.feedback);
+    this.feedbackForm.reset();
+  }
 }
